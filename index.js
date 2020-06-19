a88ed9b9f6ad64fb5e5efe4c065bf4c8c73651fe
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-06-19 02:38:25 -0400

    write files to folder

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const
 express = require('express'),
 bodyParser = require('body-parser'),
 request = require('request'),
+fs = require('fs'),
+path = require('path'),
 app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 const pageToken = process.env.pageToken
+const dir = "./jsons"
+if (!fs.existsSync(path.join("./", dir))){
+    fs.mkdirSync(path.join("./", dir));
+}
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -28,6 +34,7 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+        fs.writeFileSync(`./jsons/${webhook_event.timestamp}_${webhook_event.sender.id}.json`, webhook_event, 'utf8')
         request({
           method: 'post',
           url: "https://graph.facebook.com/v7.0/me/messages?access_token="+pageToken,
