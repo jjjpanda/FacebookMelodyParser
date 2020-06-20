73390da33e09dcd0862839544f5d346de81043c0
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-06-20 00:09:13 -0400

    updates

--- analyzeAudio.js
+++ analyzeAudio.js
@@ -17,17 +17,20 @@ module.exports = (audioUrl, id, callback) => {
         ffmpeg()
         .input(`./audio/${id}.mp4`)
         .output(`./audio/${id}.wav`)
+        .on('end', () => {
+            const buffer = fs.readFileSync(`./audio/${id}.wav`);
+            const decoded = WavDecoder.decode.sync(buffer); // get audio data from file using `wav-decoder`
+            const float32Array = decoded.channelData[0]; // get a single channel of sound
+            const detectPitch = YIN()
+            let frequency = detectPitch(float32Array)
+            console.log(frequency)
+            if(frequency == null){
+                frequency = 0;
+            }
+            callback(frequency)
+        })
         .run();
-        const buffer = fs.readFileSync(`./audio/${id}.wav`);
-        const decoded = WavDecoder.decode.sync(buffer); // get audio data from file using `wav-decoder`
-        const float32Array = decoded.channelData[0]; // get a single channel of sound
-        const detectPitch = YIN()
-        let frequency = detectPitch(float32Array)
-        console.log(frequency)
-        if(frequency == null){
-            frequency = 0;
-        }
-        callback(frequency)
+        
     })
     
     
