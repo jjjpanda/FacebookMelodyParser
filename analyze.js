3d8f6b55049f6d11343aabf7d7c9309f108de563
jjjpanda <jawpanda13@gmail.com>
2020-06-19 23:57:17 -0400

    updates

--- analyze.js
+++ analyze.js
@@ -5,7 +5,7 @@ module.exports = (webhook_event) => {
     //console.log(`./jsons/${webhook_event.message.mid}.json`)
     //fs.writeFileSync(`./jsons/${webhook_event.message.mid}.json`, JSON.stringify(webhook_event), 'utf8')
     
-    if(webhook_event.message != undefined && webhook_event.message.attachments.length == 1){
+    if(webhook_event.message != undefined && webhook_event.message.attachments != undefined && webhook_event.message.attachments.length == 1){
         if(webhook_event.message.attachments[0].type == 'audio' && webhook_event.message.attachments[0].payload != undefined){
             analyzeAudio(webhook_event.message.attachments[0].payload.url, webhook_event.message.mid, (analyzedAudio) => {
                 respondTo(webhook_event, analyzedAudio)
